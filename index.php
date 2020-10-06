9247aeb5f114e3a23b5fa6648d93a923f88e1f60
marco <marcodecrema@libero.it>
2020-10-06 15:35:52 +0200

    array domande e risposte

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Google Faq</title>
+  </head>
+  <body>
+    <?php
+      $content = [
+        [
+        "question" => "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
+        "answer" => "La recente decisione della Corte di giustizia dell'Unione europea ha profonde
+        conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno
+        il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi
+        a chiavi di ricerca che includono il proprio nome."
+        ],
+        [
+        "question" => "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?",
+        "answer" => "Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi.
+        Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro
+        e accessibili quando ne hai bisogno."
+        ],
+        [
+        "question" => "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
+        "answer" => "I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web.
+        I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati
+        di ricerca da Google non consente di rimuovere i contenuti dal Web."
+        ]
+      ]
+    ?>
+
+    <p>
+      
+    </p>
+  </body>
+</html>
